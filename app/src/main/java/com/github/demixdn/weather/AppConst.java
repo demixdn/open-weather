72d548797e795862e972a1c588c4bacd159098e6
Aleks Sander <demixdn@gmail.com>
2017-06-14 16:06:54 +0300

    app const

---
+++ app/src/main/java/com/github/demixdn/weather/AppConst.java
@@ -0,0 +1,17 @@
+package com.github.demixdn.weather;
+
+/**
+ * Created on 14.06.2017
+ * Project open-weather
+ *
+ * @author Aleks Sander
+ */
+
+public class AppConst {
+    public static final long EVICTED_PERIOD_SECONDS = 2 * 60 * 60L;
+    public static final long EVICTED_PERIOD_MILLIS = 2 * 60 * 60 * 1000L;
+
+    private AppConst() {
+        //no instance
+    }
+}
